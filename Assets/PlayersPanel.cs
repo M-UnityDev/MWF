40fdabc154c066a39b8a1a47d75985551f8802fc
M-Unity <121330925+M-UnityDev@users.noreply.github.com>
2025-03-29 16:46:58 +0500

    Waiting For Players...

--- Assets/PlayersPanel.cs
+++ Assets/PlayersPanel.cs
@@ -6,21 +6,24 @@ public class PlayersPanel : MonoBehaviour
 {
     private int PlayerCount;
     private Image Image;
+    private CanvasGroup CanvasGroup;
     public void Awake() 
     { 
         Image = GetComponent<Image>();
+        CanvasGroup = GetComponent<CanvasGroup>();
+        CanvasGroup.alpha = 1;
         Image.color = new Color(0.5f,0,0);
     }
     public void OnJoined(PlayerInput input)
     {
         PlayerCount++;
-        print(PlayerCount);
         switch (PlayerCount)
         {
             case 1:
-                Image.DOColor(new Color(0, 1, 0, 0.5f), 1);
+                Image.DOColor(new Color(0, 0.5f, 0, 0.5f), 1);
                 break;
             case 2:
+                CanvasGroup.DOFade(0, 1);
                 Image.DOColor(new Color(1, 0, 0, 0), 1);
                 break;
 
