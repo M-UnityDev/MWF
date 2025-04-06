d185af6c2c0743975c64fb00ddda08ee2f9921a8
M-Unity <121330925+M-UnityDev@users.noreply.github.com>
2025-04-07 04:04:44 +0500

    AnimationUpdate

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using DG.Tweening;
 using Unity.Cinemachine;
 using UnityEngine;
@@ -23,6 +24,7 @@ public class Movement : MonoBehaviour
     [Header("GameObjects")]
     [SerializeField] private Material[] SecondPlayerMaterial;
     [SerializeField] private Material SecondPlayerFPCMaterial;
+    [SerializeField] private Material SkyMaterial;
     [SerializeField] private AudioSource Footstep;
     public Transform AnotherPlayer;
     [SerializeField] private CinemachineBasicMultiChannelPerlin CameraShake;
@@ -30,6 +32,15 @@ public class Movement : MonoBehaviour
     [SerializeField] private GameObject FPCSkin;
     [SerializeField] private Transform CameraTransform;
     [SerializeField] private CharacterController CharacterControl;
+    [SerializeField] private Material TempMaterial;
+    [Header("Animation")]
+    [SerializeField] private GameObject Model;
+    [SerializeField] private GameObject Head;
+    [SerializeField] private GameObject Body;
+    [SerializeField] private GameObject ArmLeft;
+    [SerializeField] private GameObject ArmRight;
+    [SerializeField] private GameObject RightLeg;
+    [SerializeField] private GameObject LeftLeg;
     [Header("Debug Values")]
     private int CurrentSpeed;
     private Vector3 Velocity;
@@ -46,6 +57,7 @@ public class Movement : MonoBehaviour
     private Vector2 InputMove;
     private void Awake()
     {
+        SkyMaterial.color = Color.green;
         FindFirstObjectByType<CinemachineTargetGroup>().AddMember(transform, 10, 5);
         TempAnotherPlayers = FindObjectsByType<Movement>(FindObjectsSortMode.None);
         Vibrator = FindFirstObjectByType<VibrationDirector>();
@@ -78,7 +90,7 @@ public class Movement : MonoBehaviour
     public void OnMove(CallbackContext context)
     {
         InputMove = context.ReadValue<Vector2>();
-        TempMoveVector = PlayerPrefs.GetInt("Invert",0).Equals(0) ? new Vector3(InputMove.x, 0, InputMove.y) : new Vector3(InputMove.y, 0, InputMove.x);
+        TempMoveVector = PlayerPrefs.GetInt("Invert",0).Equals(0) ? new Vector3(InputMove.x, 0, InputMove.y) : new Vector3(InputMove.y, 0, -InputMove.x);
     }
     private void FixedUpdate() => UpdatePhysics();
     private void Update()
@@ -112,7 +124,7 @@ public class Movement : MonoBehaviour
     {
         MoveVector = IsFPC ? transform.right * TempMoveVector.x + transform.forward * TempMoveVector.z : TempMoveVector;
         Velocity.y += Physics.gravity.y * Time.deltaTime;
-        CharacterControl.Move(Time.deltaTime * ((CurrentSpeed * MoveVector) + Velocity));
+        if (CharacterControl.enabled) CharacterControl.Move(Time.deltaTime * ((CurrentSpeed * MoveVector) + Velocity));
     }
     private void UpdateCollisons()
     {
@@ -121,18 +133,7 @@ public class Movement : MonoBehaviour
         {
             if (Item.CompareTag("Finish"))
             {
-                Destroy(Item.gameObject);
-                FPC.SetActive(true);
-                IsFPC = true;
-                GetComponent<MeshRenderer>().enabled = false;
-                AnotherPlayer.GetComponent<Movement>().DistanceToWalkFuckYou = int.MaxValue;
-                
-                Destroy(Camera.main.transform.parent.GetComponent<CinemachineCamera>());
-                Destroy(Camera.main.transform.parent.GetComponent<CinemachineGroupFraming>());
-                Destroy(Camera.main.transform.parent.GetComponent<CinemachineDecollider>());
-                Destroy(Camera.main.transform.parent.GetComponent<CameraTriggerDirector>());
-                Camera.main.gameObject.SetActive(false);
-                Cursor.lockState = CursorLockMode.Locked;
+                StartCoroutine(WinCutScene(Item.gameObject));
                 return;
             }
             else if (Item.transform.Equals(AnotherPlayer) && IsFPC && !IsWin)
@@ -154,7 +155,8 @@ public class Movement : MonoBehaviour
     private void UpdateSound()
     {
         Footstep.mute = !CharacterControl.isGrounded || InputMove.Equals(Vector2.zero) || !IsFPC;
-        Footstep.pitch = IsRunning && IsFPC ? 2 : 1;
+        Footstep.pitch = IsRunning && IsFPC ? 1 : 0.75f;
+        if (IsFPC) Footstep.panStereo = Random.Range(-1f,1f);
     }
     private void UpdateRotation()
     {
@@ -165,8 +167,8 @@ public class Movement : MonoBehaviour
             switch (IsRunning, TempMove)
             {
                 case (true,true):
-                    CameraShake.AmplitudeGain = 1;
-                    Vibrator.StartVibrate(1,1);
+                    CameraShake.AmplitudeGain = 0.5f;
+                    Vibrator.StartVibrate(20,20);
                 break;
                 case (false,true):
                     CameraShake.AmplitudeGain = 0.25f;
@@ -180,9 +182,60 @@ public class Movement : MonoBehaviour
         }
         else transform.localRotation = Quaternion.Lerp(transform.rotation, TempMoveVector.sqrMagnitude > 0 ? Quaternion.LookRotation(TempMoveVector) : transform.rotation, RotationSpeed * Time.deltaTime);
     }
-    public void UpdateOutline()
+    private void UpdateOutline()
     {
         if (IsFPC)
             outline.SetActive(Physics.RaycastAll(transform.position, AnotherPlayer.position - transform.position, Mathf.Sqrt(Vector3.SqrMagnitude(AnotherPlayer.position - transform.position)), Walls).Length > 0);
+    }
+    private IEnumerator WinCutScene(GameObject Item)
+    {
+        Destroy(Item.gameObject);
+        transform.DORotate(Vector3.up*180,1);
+        Camera.main.transform.parent.GetComponent<CinemachineTargetGroup>().RemoveMember(AnotherPlayer);
+        UpdateJump();
+        yield return new WaitForSeconds(0.45f);
+        CharacterControl.enabled = false;
+        FPC.SetActive(true);
+        Model.SetActive(true);
+        LeftLeg.SetActive(true);
+        RightLeg.SetActive(true);
+        Model.transform.DOLocalMoveY(-1.28f,1).SetEase(Ease.InOutCubic);
+        yield return new WaitForSeconds(1);
+        Body.SetActive(true);
+        ArmLeft.SetActive(true);
+        ArmRight.SetActive(true);
+        Model.transform.DOLocalMoveY(-2,1);
+        yield return new WaitForSeconds(1);
+        ArmLeft.transform.DOLocalMoveX(-0.3125f,1).SetEase(Ease.InOutCubic);
+        ArmRight.transform.DOLocalMoveX(0.3125f,1).SetEase(Ease.InOutCubic);
+        yield return new WaitForSeconds(1);
+        Head.SetActive(true);
+        Head.transform.localScale = Vector3.one*2;
+        GetComponent<MeshRenderer>().enabled = false;
+        Head.transform.DOScale(Vector3.one,1);
+        yield return new WaitForSeconds(1);
+        Model.transform.DOLocalMoveY(-0.5f,1);
+        transform.DORotate(Vector3.zero,1);
+        yield return new WaitForSeconds(0.5f);
+        if (Velocity.y < 0) Velocity.y = -2;
+        CharacterControl.enabled = true;
+        CameraTransform.gameObject.SetActive(true);
+        yield return new WaitForSeconds(2);
+        Destroy(Camera.main.transform.parent.GetComponent<CinemachineCamera>());
+        Destroy(Camera.main.transform.parent.GetComponent<CinemachineGroupFraming>());
+        Destroy(Camera.main.transform.parent.GetComponent<CinemachineTargetGroup>());
+        Destroy(Camera.main.transform.parent.GetComponent<CinemachineDeoccluder>());
+        Destroy(Camera.main.transform.parent.GetComponent<CameraTriggerDirector>());
+        IsFPC = true;
+        AnotherPlayer.GetComponent<Movement>().DistanceToWalkFuckYou = int.MaxValue;
+        SkyMaterial.DOColor(Color.red,1);
+        TempMaterial.color = new Color(0.5f,1,0.5f);
+        TempMaterial.DOColor(new Color(1,0.5f,0.5f),1).OnUpdate(() => {RenderSettings.ambientLight = TempMaterial.color;});
+        Camera.main.transform.parent = null;
+        GameObject.Find("OutlineCamera").transform.parent = null;
+        //FPCamera.SetActive(true);
+        //FPCameraOutline.SetActive(true);
+        Cursor.lockState = CursorLockMode.Locked;
+
     }
 }
--- Assets/Scripts/PlayersPanel.cs
+++ Assets/Scripts/PlayersPanel.cs
@@ -1,15 +1,18 @@
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using UnityEngine.InputSystem;
 public class PlayersPanel : MonoBehaviour
 {
     private int PlayerCount;
     private VibrationDirector Vibrator;
+    private PlayerInputManager PIM;
     private Image Image;
     private CanvasGroup CanvasGroup;
     public void Awake() 
     { 
         Vibrator = FindFirstObjectByType<VibrationDirector>();
+        PIM = FindFirstObjectByType<PlayerInputManager>();
         Image = GetComponent<Image>();
         CanvasGroup = GetComponent<CanvasGroup>();
         CanvasGroup.alpha = 1;
@@ -27,6 +30,7 @@ public class PlayersPanel : MonoBehaviour
             case 2:
                 CanvasGroup.DOFade(0,1);
                 Image.DOColor(new Color(0,1,0,0),1).OnComplete(() => {Destroy(Image);});
+                PIM.DisableJoining();
                 break;
         }
     }
